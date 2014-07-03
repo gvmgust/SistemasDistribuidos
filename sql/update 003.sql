e5eb40dfa795adcf201c5057b82a34191d7a089a
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-03 17:16:33 -0400

    Renombrando tablas para darle un uso mas practico

---
+++ sql/update 003.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `candidato` CHANGE `id_cand` `id_opc` INT(3) NOT NULL, CHANGE `Candidato` `Opciones` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL; 
+RENAME TABLE `candidato` TO `sistemasdistribuidos`.`opciones`; 
