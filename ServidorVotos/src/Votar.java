431cf1239b39b6685d3433ea51bd3d0a6916416a
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-04 08:13:00 -0400

    Agregando metodos faltantes a objetos remotos

--- ServidorVotos/src/Votar.java
+++ ServidorVotos/src/Votar.java
@@ -1,6 +1,7 @@
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,5 +14,7 @@ import java.rmi.RemoteException;
  * @author Gustavo Vargas M
  */
 public interface Votar  extends Remote {
-    public void emitirVoto(String s)throws RemoteException;
+    public void emitirVoto(String v,String ci)throws RemoteException;
+    public int loguea(String ci, String pass)throws RemoteException;
+    public ArrayList<String> listarOpciones();
 }
--- ServidorVotos/src/VotarImp.java
+++ ServidorVotos/src/VotarImp.java
@@ -1,26 +1,81 @@
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Gustavo Vargas M
  */
 public class VotarImp extends UnicastRemoteObject implements Votar {
 
-    public VotarImp()throws java.rmi.RemoteException{
+    public VotarImp() throws java.rmi.RemoteException {
         super();
     }
-    
+
     @Override
-    public void emitirVoto(String s) {
-        
+    public void emitirVoto(String v,String ci) {
+        int id_opc = -1;
+        ResultSet rs = Main.con.consultar(SQL.buscarOpcion(v));
+        try {
+            while(rs.next()){
+                id_opc = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VotarImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Main.con.ejecutar(SQL.updateVotos(id_opc));
+        Main.con.ejecutar(SQL.registrarVoto(ci));
+        Main.interfazVotos.actualizarTablas();
     }
-    
+
+    @Override
+    public int loguea(String ci, String pass) throws RemoteException {
+        int activo = -1;
+        ResultSet rs = Main.con.consultar(SQL.habilitado(ci, pass));
+        try {
+            while (rs.next()) {
+                activo = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VotarImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(activo==1){
+            rs = Main.con.consultar(SQL.verificarSiVoto(ci));
+            try {
+                while(rs.next()){
+                    activo = 2;
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(VotarImp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return activo;
+    }
+
+    @Override
+    public ArrayList<String> listarOpciones() {
+        ArrayList<String> opciones = new ArrayList();
+        ResultSet rs = Main.con.consultar(SQL.listarOpciones());
+        try {
+            while (rs.next()) {
+                opciones.add(rs.getString(1));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VotarImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return opciones;
+    }
+
+
 }
