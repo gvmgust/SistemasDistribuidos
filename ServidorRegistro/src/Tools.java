3b3eb3e9979a0358a7132a1c7f61bf3219b6d0d7
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-04 04:05:34 -0400

    Creando nueva biblioteca de herramientas (herramienta nueva que permite realizar preguntas de confirmacion)

---
+++ ServidorRegistro/src/Tools.java
@@ -0,0 +1,31 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Gustavo Vargas M
+ */
+public abstract class Tools {
+
+    public static boolean pregunta(String pregunta) {
+        int seleccion = JOptionPane.showOptionDialog(
+                null, pregunta,
+                "Seleccione una opción", // Título
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null, // null para icono por defecto.
+                new Object[]{"Si", "No"}, // null para YES, NO y CANCEL
+                "Si");
+        if (seleccion != -1) {
+            if ((seleccion + 1) == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
