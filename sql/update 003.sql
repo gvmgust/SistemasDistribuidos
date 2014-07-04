1ab3293d80ebd823cf8184c2ffbda873f02f203f
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-03 22:46:43 -0400

    Modificacion a la base de datos

--- sql/update 003.sql
+++ sql/update 003.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `candidato` CHANGE `id_cand` `id_opc` INT(3) NOT NULL, CHANGE `Candidato` `Opciones` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL; 
 RENAME TABLE `candidato` TO `sistemasdistribuidos`.`opciones`; 
+ALTER TABLE `postulantes` CHANGE `id_cand` `id_opc` INT(8) NOT NULL; 
