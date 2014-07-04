e28108b638551eeefcc3cedd188707fdceb24374
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-03 22:49:42 -0400

    Agregando funcionalidades de mostrar en tiempo real las personas registradas en los clientes remotos

--- ServidorRegistro/src/InterfazSR.java
+++ ServidorRegistro/src/InterfazSR.java
@@ -1,12 +1,16 @@
 
 import java.awt.Color;
+import java.awt.Frame;
 import java.rmi.Naming;
 import java.rmi.NoSuchObjectException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,10 +28,12 @@ public class InterfazSR extends javax.swing.JFrame {
      */
     public InterfazSR() {
         initComponents();
+        this.setExtendedState(Frame.MAXIMIZED_BOTH);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-        jButton1.setForeground(Color.GREEN);
+        jButton1.setForeground(Color.BLUE);
         this.setTitle("Registrar");
+        this.actualizarVistaTablas();
     }
 
     /**
@@ -41,6 +47,8 @@ public class InterfazSR extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -54,21 +62,28 @@ public class InterfazSR extends javax.swing.JFrame {
             }
         });
 
+        jTable1.setModel(modelo);
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(263, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 375, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jButton1)
-                .addContainerGap(242, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -95,12 +110,48 @@ public class InterfazSR extends javax.swing.JFrame {
         iniciarFinalizar();
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    public void iniciarFinalizar() {
+    public void actualizarVistaTablas() {
+        modelo.setColumnCount(0);
+        modelo.setRowCount(0);
+        modelo.addColumn("CI");
+        modelo.addColumn("Apellido Paterno");
+        modelo.addColumn("Apellido Materno");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Direccion");
+        modelo.addColumn("F. Inscripcion");
+        jTable1.getColumnModel().getColumn(0).setMaxWidth(80);
+        jTable1.getColumnModel().getColumn(0).setMinWidth(60);
+        jTable1.getColumnModel().getColumn(1).setMaxWidth(200);
+        jTable1.getColumnModel().getColumn(1).setMinWidth(150);
+        jTable1.getColumnModel().getColumn(2).setMaxWidth(200);
+        jTable1.getColumnModel().getColumn(2).setMinWidth(150);
+        jTable1.getColumnModel().getColumn(3).setMaxWidth(200);
+        jTable1.getColumnModel().getColumn(3).setMinWidth(150);
+        jTable1.getColumnModel().getColumn(5).setMaxWidth(200);
+        jTable1.getColumnModel().getColumn(5).setMinWidth(150);
+        ResultSet rs = Main.con.consultar(SQL.mostrarUltimosNHabilitados(20));
+        try {
+            while (rs.next()) {
+                Object[] vector = new Object[7];
+                vector[0] = rs.getString(1);
+                vector[1] = rs.getString(2);
+                vector[2] = rs.getString(3);
+                vector[3] = rs.getString(4);
+                vector[4] = rs.getString(5);
+                vector[5] = rs.getString(6);
+                modelo.addRow(vector);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InterfazSR.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void iniciarFinalizar() {
         try {
             if (estado) {
                 UnicastRemoteObject.unexportObject(registry, true);
                 jButton1.setText("Iniciar Servidor");
-                jButton1.setForeground(Color.GREEN);
+                jButton1.setForeground(Color.BLUE);
                 estado = false;
             } else {
                 RegistrarImpl registrar;
@@ -116,10 +167,17 @@ public class InterfazSR extends javax.swing.JFrame {
         }
     }
 
-    public boolean estado = false;
-    Registry registry;
+    private boolean estado = false;
+    private Registry registry;
+    private DefaultTableModel modelo = new DefaultTableModel() {
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
