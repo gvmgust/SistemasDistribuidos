96e0dc6b5f3962b7d0e2d2a8c668fac68eebc5ac
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-04 08:13:24 -0400

    Agregando consultas a la biblioteca de consultas

--- ServidorVotos/src/SQL.java
+++ ServidorVotos/src/SQL.java
@@ -20,4 +20,26 @@ public abstract class SQL {
                 + "WHERE id_ele = '" + Main.Eleccion + "'";
     }
 
+    public static String buscarOpcion(String opcion) {
+        return "SELECT id_opc FROM opciones WHERE opciones = '" + opcion + "'";
+    }
+
+    public static String updateVotos(int id_opc) {
+        return "UPDATE postulantes SET votos=votos+1 WHERE id_opc = '" + id_opc + "' AND id_ele = '" + Main.Eleccion + "'";
+    }
+
+    public static String registrarVoto(String CI) {
+        return "INSERT INTO votacion(id_ele,ci)VALUES('" + Main.Eleccion + "','" + CI + "');";
+    }
+
+    public static String verificarSiVoto(String ci) {
+        return "SELECT COUNT(id_ele) FROM votacion WHERE ci = '" + ci + "' AND id_ele = '" + Main.Eleccion + "';";
+    }
+
+    public static String contarVotos() {
+        return "SELECT opciones.`Opciones`, postulantes.`votos` \n"
+                + "FROM opciones INNER JOIN postulantes\n"
+                + "ON opciones.id_opc = postulantes.`id_opc`\n"
+                + "WHERE id_ele = '" + Main.Eleccion + "'";
+    }
 }
